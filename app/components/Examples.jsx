ff7413da3b16362529c43af9902ab08b531f0bce
Vaibhav <vaibhav.verma2697@gmail.com>
2018-03-01 12:02:08 +0530

    Update copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,7 @@ var React=require('react')
 var Examples=React.createClass({
         render:function(){
                 return(
-                        <h3>Examples Component</h3>
+                        <h3>Examples Component !!</h3>
                 )
         }
 });
