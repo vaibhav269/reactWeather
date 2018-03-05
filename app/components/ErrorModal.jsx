547ad2da052cb9089836e361399131317023a038
Vaibhav <vaibhav.verma2697@gmail.com>
2018-03-05 16:19:51 +0530

    Fix error modal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React=require('react');
+var ReactDOM=require('react-dom');
+var ReactDOMServer=require('react-dom/server');
 var ErrorModal=React.createClass({
         getDefaultProps:function(){
                 return{
@@ -10,19 +12,26 @@ var ErrorModal=React.createClass({
                         errorMessage:React.PropTypes.string.isRequired
         },
         componentDidMount:function(){
+                var modalMarkup=(
+                        <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+                                <h4>{this.props.title}</h4>
+                                <p>{this.props.errorMessage}</p>
+                                <p>
+                                        <button className="button hollow" data-close="">
+                                                Okay
+                                        </button>
+                                </p>
+                        </div>
+                );
+                var $modal=$(ReactDOMServer.renderToString(modalMarkup));
+                $(ReactDOM.findDOMNode(this)).html($modal);
+                
                 var modal =  new Foundation.Reveal($('#error-modal'));
                 modal.open();
         },
-        render:function(){
+        render:function(){1
                         return(
-                                <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-                                        <h4>{this.props.title}</h4>
-                                        <p>{this.props.errorMessage}</p>
-                                        <p>
-                                                <button className="button hollow" data-close="">
-                                                        Okay
-                                                </button>
-                                        </p>
+                                <div>
                                 </div>
                         )
         }
